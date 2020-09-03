3c62e9cbbd1c1dfc529a8d38f66904bb2261e724
jhonatan.valencia <tatanvalenciaarango@gmail.com>
2020-09-02 20:11:28 -0500

    Creación de migraciones Users, categories y games

--- larapp/database/migrations/2014_10_12_000000_create_users_table.php
+++ larapp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
             $table->string('email')->unique();
+            $table->bigInteger('phone');
+            $table->date('birthdate');
+            $table->string('gender');
+            $table->string('address');
+            $table->string('photo')->default('imgs/no-photo.png');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('Customer');
             $table->rememberToken();
             $table->timestamps();
         });
