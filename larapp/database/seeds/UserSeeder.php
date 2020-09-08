d458388b71afc9f29a28f8ee1e6295defa351e0d
jhonatan.valencia <tatanvalenciaarango@gmail.com>
2020-09-07 20:08:20 -0500

    Creación de modelos User, Category y Game. Creación y ejecución de UserSeed.

---
+++ larapp/database/seeds/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'fullname'      => 'Jeremias Springfield',
+            'email'         => 'jeremias@gmail.com',
+            'phone'         => 321658974,
+            'birthdate'     => '1970-08-07',
+            'gender'        => 'Male',
+            'address'       => 'Evergreen Avenue',
+            'password'      => bcrypt('admin'),
+            'role'          => 'Admin',
+            'created_at'    => now(),
+        ]);
+
+        $usr = new User;
+        $usr->fullname      = 'Homer Simpson';
+        $usr->email         = 'HomerSimpson@gmail.com';
+        $usr->phone         = 3256987456;
+        $usr->birthdate     = '1988-10-10';
+        $usr->gender        = 'Male';
+        $usr->address       = 'Evergreen Avenue';
+        $usr->password      = bcrypt('123');
+        $usr->save();
+    }
+}
